998a414cadac766c6998f2d4f5c419bd74d7ad7e
fkdkskdfjfk <yljw225@naver.com>
2024-03-24 21:24:57 +0900

    metadata

---
+++ app/(home)/page.jsx
@@ -0,0 +1,17 @@
+// import styles from "./page.module.css";
+import { OpenGraph } from "../layout";
+
+export const metadata = {
+  // title: "qwer",
+  OpenGraph: {
+    ...OpenGraph
+  }
+};
+
+export default function Home() {
+
+  
+  return (
+    <div>홈</div>
+  );
+}
