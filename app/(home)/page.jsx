a7fcff66c6cff7305dac53871f06070fded6093b
fkdkskdfjfk <yljw225@naver.com>
2024-03-25 01:26:31 +0900

    api 성공

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -8,15 +8,16 @@ export const metadata = {
   // }
 };
 
-export const API_URL = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=ero0PCiw0xS0m5XbHGdRNe4XLQfmyRSHVU2pPJQ7xx%2B%2BC2lnsL7zametsqSaIqJNoTXnkKCdi2l5oIxMKgLR%2FQ%3D%3D&numOfRows=10&pageNo=1&dataType=JSON&base_date=20210628&base_time=0600&nx=55&ny=127";
+export const API_URL = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=ero0PCiw0xS0m5XbHGdRNe4XLQfmyRSHVU2pPJQ7xx%2B%2BC2lnsL7zametsqSaIqJNoTXnkKCdi2l5oIxMKgLR%2FQ%3D%3D&numOfRows=12&pageNo=1&dataType=JSON&base_date=20240324&base_time=0500&nx=55&ny=127";
 
 const getWeather = async () => {
-  const response = await fetch(API_URL);
-  console.log(response);
-  // const json = await JSON.parse(response);
-  const json = await response.json();
-  console.log(json);
-  return JSON.stringify(json);
+  try {
+    const response = await fetch(API_URL);
+    const json = await response.json();
+    return JSON.stringify(json);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export default async function Home() {
@@ -25,6 +26,7 @@ export default async function Home() {
   return (
     <>
       <div>{weather}</div>
+      <div>{weather.body}</div>
     </>
   );
 }
