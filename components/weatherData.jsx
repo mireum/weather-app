7b8c00eb448ad5d8b80cba0b327b14235b5da847
fkdkskdfjfk <yljw225@naver.com>
2024-03-28 23:19:05 +0900

    값 구현

--- components/weatherData.jsx
+++ components/weatherData.jsx
@@ -1,25 +1,25 @@
 export default function WeatherData({data}) {
 
   console.log(data);
-  let cloud = data[5].fcstValue;
 
-  if (cloud < 3) {
-    cloud = '맑음';
-  } else if (cloud < 6) {
-    cloud = '구름 있음';
-  } else if (cloud < 8) {
-    cloud = '구름 많음';
-  } else {
-    cloud = '흐림';
-  }
   // 현재 기온
   const TMP = data[0].fcstValue;
   // 풍향
   const VEC = data[3].fcstValue;
   // 풍속
   const WSD = data[4].fcstValue;
-  // 하늘상태
-  const SKY = cloud;
+  // 구름
+  let SKY = data[5].fcstValue;
+  if (SKY < 3) {
+    SKY = '맑음';
+  } else if (SKY < 6) {
+    SKY = '구름 있음';
+  } else if (SKY < 8) {
+    SKY = '구름 많음';
+  } else {
+    SKY = '흐림';
+  }
+
   // 강수확률
   const POP = data[7].fcstValue;
   // 1시간 강수량
@@ -33,8 +33,8 @@ export default function WeatherData({data}) {
         <li>현재 기온: {TMP} &#8451;</li>
         <li>풍향: {VEC} deg</li>
         <li>풍속: {WSD}m/s</li>
-        <li>구름: {cloud}</li>
-        <li>강수 확률: {POP}%</li>
+        <li>구름: {SKY}</li>
+        <li>{POP == '강수없음' ? '' : `강수 확률: ${POP}%`}</li>
         <li>강수량: {PCP}mm</li>
         <li>습도: {REH} %</li>
       </ul>
