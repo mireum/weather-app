d5aa9dba8fa60a55d3f04f3f40f571f5dcca0749
fkdkskdfjfk <yljw225@naver.com>
2024-04-04 13:55:55 +0900

    전국 지도

--- components/KakaoMap.jsx
+++ components/KakaoMap.jsx
@@ -1,13 +1,29 @@
 import Script from 'next/script';
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
-const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&autoload=false`;
+const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&autoload=false&libraries=services,clusterer`;
+
 
 const KakaoMap = () => {
   return (
     <>
       <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
-      <Map center={{ lat: 33.450701, lng: 126.570667 }} style={{ width: '100%', height: '70%' }}>
+      <Map 
+        center={{ lat: 36.150701, lng: 127.670667 }} 
+        style={{ width: '100%', height: '80%' }}
+        level={12}
+        zoomable={false}
+      >
+        {/* 서울 */}
+        <MapMarker 
+          position= {{
+            lat: 37.564213,
+            lng: 126.981698,
+          }}
+        >
+          <div style={{ height: "30px", padding: "3px" }}>서울 기온 15&#8451;</div>
+        </MapMarker>
+
       </Map>
     </>
   );
